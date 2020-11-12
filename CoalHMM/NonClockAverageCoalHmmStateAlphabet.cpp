d2af16116495c9b650532581a48340b4655857f9
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2020-11-12 22:22:30 +0100

    Updated 'Unclock' model
    Now two species can have an additional error term.

--- CoalHMM/NonClockAverageCoalHmmStateAlphabet.cpp
+++ CoalHMM/NonClockAverageCoalHmmStateAlphabet.cpp
@@ -24,46 +24,93 @@
 using namespace bpp;
 using namespace std;
 
-NonClockAverageCoalHmmStateAlphabet::NonClockAverageCoalHmmStateAlphabet(AverageCoalHmmStateAlphabet* alphabet, const string& species, double error):
+NonClockAverageCoalHmmStateAlphabet::NonClockAverageCoalHmmStateAlphabet(
+		AverageCoalHmmStateAlphabet* alphabet,
+	       	const string& species, double error):
   AbstractParametrizable(""),
   alphabet_(alphabet),
-  speciesWithError_(species),
-  error_(error),
+  speciesWithError1_(species),
+  speciesWithError2_(""),
+  error1_(error),
+  error2_(0),
+  hasSecondError_(false),
   brlenParameters_()
 {
   trees_.resize(alphabet_->getNumberOfStates());
   brlenParameters_.resize(alphabet_->getNumberOfStates());
-  for (unsigned int i = 0; i < alphabet_->getNumberOfStates(); ++i) {
+  for (size_t i = 0; i < alphabet_->getNumberOfStates(); ++i) {
     trees_[i] = alphabet_->getState(i).clone();
     brlenParameters_[i] = alphabet_->getBranchLengthParametersForState(i);
   }
   species_ = alphabet->getSpeciesNames();
 
   addParameters_(alphabet_->getParameters());
-  addParameter_(new Parameter(speciesWithError_ + "_error", error_, Parameter::R_PLUS));
+  addParameter_(new Parameter(speciesWithError1_ + "_error", error1_, Parameter::R_PLUS));
+  fireParameterChanged(getParameters());
+}
+
+NonClockAverageCoalHmmStateAlphabet::NonClockAverageCoalHmmStateAlphabet(
+		AverageCoalHmmStateAlphabet* alphabet,
+	       	const string& species1,
+	       	const string& species2,
+	       	double error1,
+	       	double error2):
+  AbstractParametrizable(""),
+  alphabet_(alphabet),
+  speciesWithError1_(species1),
+  speciesWithError2_(species2),
+  error1_(error1),
+  error2_(error2),
+  hasSecondError_(true),
+  brlenParameters_()
+{
+  trees_.resize(alphabet_->getNumberOfStates());
+  brlenParameters_.resize(alphabet_->getNumberOfStates());
+  for (size_t i = 0; i < alphabet_->getNumberOfStates(); ++i) {
+    trees_[i] = alphabet_->getState(i).clone();
+    brlenParameters_[i] = alphabet_->getBranchLengthParametersForState(i);
+  }
+  species_ = alphabet->getSpeciesNames();
+
+  addParameters_(alphabet_->getParameters());
+  addParameter_(new Parameter(speciesWithError1_ + "_error", error1_, Parameter::R_PLUS));
+  addParameter_(new Parameter(speciesWithError2_ + "_error", error2_, Parameter::R_PLUS));
   fireParameterChanged(getParameters());
 }
 
 void NonClockAverageCoalHmmStateAlphabet::fireParameterChanged(const ParameterList& params)
 {
   alphabet_->matchParametersValues(params);
-  error_ = getParameterValue(speciesWithError_ + "_error");
+  error1_ = getParameterValue(speciesWithError1_ + "_error");
+  if (hasSecondError_){
+    error2_ = getParameterValue(speciesWithError2_ + "_error");
+  }
 
   //Update topologies and branch lengths:
-  for (unsigned int i = 0; i < alphabet_->getNumberOfStates(); ++i) {
+  for (size_t i = 0; i < alphabet_->getNumberOfStates(); ++i) {
     *trees_[i] = alphabet_->getState(i);
-    Node* node = trees_[i]->getNode(speciesWithError_);
-    node->setDistanceToFather(node->getDistanceToFather() + error_);
+    Node* node = trees_[i]->getNode(speciesWithError1_);
+    node->setDistanceToFather(node->getDistanceToFather() + error1_);
     brlenParameters_[i].matchParametersValues(alphabet_->getBranchLengthParametersForState(i));
     brlenParameters_[i].setParameterValue(
         "BrLen" + TextTools::toString(node->getId()), node->getDistanceToFather()); 
+    if (hasSecondError_) {
+      Node* node2 = trees_[i]->getNode(speciesWithError2_);
+      node2->setDistanceToFather(node2->getDistanceToFather() + error2_);
+      brlenParameters_[i].matchParametersValues(alphabet_->getBranchLengthParametersForState(i));
+      brlenParameters_[i].setParameterValue(
+          "BrLen" + TextTools::toString(node2->getId()), node2->getDistanceToFather()); 
+    }
   }
 }
 
 void NonClockAverageCoalHmmStateAlphabet::printUserFriendlyParameters(bpp::OutputStream& out) const
 {
   alphabet_->printUserFriendlyParameters(out);
-  (out << speciesWithError_ << "_error = " << error_).endLine();
+  (out << speciesWithError1_ << "_error = " << error1_).endLine();
+  if (hasSecondError_) {
+    (out << speciesWithError2_ << "_error = " << error2_).endLine();
+  }
 }
 
 
